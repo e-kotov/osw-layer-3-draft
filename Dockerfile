5c709cb48d1458fd99171cc5c17e6c3a7891b1ed
Egor Kotov <kotov.egor@gmail.com>
2025-03-07 14:18:34 +0100

    add more detailed explanations to source files

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM ghcr.io/e-kotov/osw-layer-3-draft:latest
+# this line above instructs mybinder.org to use our container that was pre-built and uploaded to the github container registry as a base layer
 
 # Copy everything to /home/rstudio
 COPY --chown=rstudio . /home/rstudio
 
-# Remove the files you don’t want in your final image
+# Remove the files related to renv, so that renv does not take over the project
+# We have (hopefully) all the packages already installed and burned into the container image
 RUN rm -rf /home/rstudio/renv \
            /home/rstudio/renv.lock \
            /home/rstudio/.Rprofile
--- _targets.R
+++ _targets.R
@@ -1,5 +1,8 @@
-# targets::tar_make()
-# targets::tar_visnetwork()
+# targets::tar_make() # build the project
+# targets::tar_visnetwork() # visualise the workflow
+# x <- targets::tar_read(main_dataset) # load the saved output of a targets step into var x (e.g. for interactive testing)
+# targets::tar_load(main_dataset) # load the saved output of a targets step with its original saved name `main_dataset` into current session's environemnt (e.g. for interactive testing)
+# targets::tar_destroy() delete all saved results of all targets steps
 
 # Load packages required to define the pipeline:
 library(targets)
@@ -7,7 +10,7 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  format = "qs"
+  format = "qs" # targets will use `qs2` R package to efficiently save and load results of each step
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -18,7 +21,7 @@ list(
   # get data from osf ------------------------------------------------------
   tar_target(
     name = main_dataset,
-    packages = c("osfr", "dplyr", "fs"),
+    packages = c("osfr", "dplyr", "fs"), # this way we only use the packages we need in this particular step, keeping the environment clean of unneeded packages taht may be used in other steps
     command = get_osf_data(
       tmp_dir = "data/tmp"
     )
